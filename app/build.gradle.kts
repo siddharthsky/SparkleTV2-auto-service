b33ed1bc979a3714ef61b1b65ae004ba1459f6df
Siddharth Kamble <siddharthsk7t@gmail.com>
2023-12-10 22:50:08 +0530

    init build

---
+++ app/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.skylake.siddharthsky.sparkletv2"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.skylake.siddharthsky.sparkletv2"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.leanback:leanback:1.0.0")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+}
---
+++ app/src/main/java/com/skylake/siddharthsky/sparkletv2/MainActivity.kt
@@ -0,0 +1,83 @@
+package com.skylake.siddharthsky.sparkletv2
+
+import android.content.ComponentName
+import android.content.Intent
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Check if the site is up or down
+        CheckSiteStatusTask().execute()
+    }
+
+    private inner class CheckSiteStatusTask : AsyncTask<Void, Void, Boolean>() {
+        override fun doInBackground(vararg params: Void?): Boolean {
+            return isSiteReachable("localhost", 5001, 2000)
+        }
+
+        override fun onPostExecute(result: Boolean) {
+            if (result) {
+                showToast("Site is up")
+                openApp("se.hedekonsult.sparkle")
+            } else {
+                showToast("Site is down")
+
+                openApp("com.termux")
+
+                // Wait for another 5.5 seconds before opening the second app
+                Thread.sleep(5000)
+
+                showToast("Site is up OOOOO?")
+
+                openApp("se.hedekonsult.sparkle")
+            }
+        }
+    }
+
+    private fun isSiteReachable(host: String, port: Int, timeout: Int): Boolean {
+        return try {
+            val socket = Socket()
+            socket.connect(InetSocketAddress(host, port), timeout)
+            socket.close()
+            true
+        } catch (e: IOException) {
+            false
+        }
+    }
+
+    private fun openApp(packageName: String) {
+        val launchIntent = packageManager.getLaunchIntentForPackage(packageName)
+        if (launchIntent != null) {
+            startActivity(launchIntent)
+        } else {
+            // If the app is not installed, open its page on the Google Play Store
+            val playStoreIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("market://details?id=$packageName")
+            )
+            startActivity(playStoreIntent)
+            showToast("App not installed: $packageName")
+            // You may handle this case differently, e.g., redirect to the app's store page
+        }
+    }
+
+
+
+    private fun showToast(message: String) {
+        runOnUiThread {
+            Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
